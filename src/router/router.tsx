e677b4b68430d9e603d909305c8761c7104ebb3d
niduranga <nidurangajayarathna@gmail.com>
2026-07-20 14:16:01 +0530

    Create RooteLayout and add it to the base route element

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,9 +1,16 @@
 import {createBrowserRouter} from "react-router";
 import Home from "../pages/Home.tsx";
+import RootLayout from "../layout/RootLayout.tsx";
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <Home/>
+        element: <RootLayout/>,
+        children: [
+            {
+                path: '/',
+                element: <Home/>
+            }
+        ]
     }
 ])
