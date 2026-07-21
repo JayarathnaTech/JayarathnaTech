05aa5782e192c9df021e8504ae93f7496af6ad3b
niduranga <nidurangajayarathna@gmail.com>
2026-07-21 11:18:17 +0530

    Fix not scroll top after visit new page

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Outlet} from "react-router";
 import NavigationBar from "../components/NavigationBar.tsx";
+import ScrollToTop from "../components/ScrollToTop.tsx";
 
 const RootLayout :React.FC = () => {
     return (
         <div className="min-h-screen bg-slate-950 text-slate-100 flex flex-col font-sans antialiased selection:bg-indigo-500/30 selection:text-indigo-200">
+            <ScrollToTop />
             <NavigationBar/>
             <main className="flex-grow">
                 <Outlet/>
